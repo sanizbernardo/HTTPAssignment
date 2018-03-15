71534dc5369085ce10b1711fac54d268bf3224ec
Stephen Huang <stephenhuang@MacBook-Pro.local>
2018-03-15 18:11:52 +0100

    Still trying to clean up code and implement all aspects, DELETED the workspace.xml

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,140 @@
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 class HTTPClient {
+
+
+
     public static void main(String args[]) throws Exception
     {
-        if (args.length != 3)
-            throw new Exception("Incorrect input");
-        String command = args[0], uri = args[1], port = args[2];
-        byte[] bufferText = new byte[4096];
-
-        //BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-        //Socket clientSocket = new Socket("www.google.com", 80);
-        System.out.println("ARGUMENTEN: 1e"+args[0]+" en 2e: "+args[1]+" en 3e: "+args[2]);
-        Socket clientSocket = new Socket(args[1], Integer.valueOf(args[2]));
-        InputStream inputStream = clientSocket.getInputStream();
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        //BufferedReader inFromServer = new BufferedReader(new InputStreamReader(inputStream));
-        //String sentence = inFromUser.readLine();
-        System.out.println("InputStream Available "+inputStream.available());
-        outToServer.writeBytes(args[0] + " /index.html HTTP/1.1\n");
-        outToServer.writeBytes("Host: "+args[1]+"\n");
-        //outToServer.writeBytes("GET /index.html HTTP/1.1 \n");
-        outToServer.writeBytes("\n");
+        /*
+         * Variables
+         */
         boolean ended = false;
         String htmlInput = "";
         String substr = "";
-        inputStream.read(bufferText);
-        String str = new String(bufferText);
-        System.out.println("string: "+str);
-        System.out.println("---------------------");
+        String byteString= "";
+
+        // Creating byte array for incoming bytes
+        byte[] bytes;
+
+        // Creating new html template
+        File htmlTemplateFile = new File("src/template.html");
+
+        // Creating new array list for images in html file
         ArrayList<String> images = new ArrayList<String>();
-        /*while (!ended) {
+
+        // Checking for correct input of user
+        if (args.length != 3)
+            throw new Exception("Incorrect input");
+
+        // Initializing the variables from user arguments
+        String HTTPCommand = args[0];
+        URI uri = new URI(args[1]);
+        String path = uri.getPath();
+        int port = Integer.valueOf(args[2]);
+
+        /*
+         * Connection
+         */
+
+        // Creating new socket to connect to URI with correct port
+        Socket clientSocket = new Socket(path,port);
+
+        // Get the input stream from the clientsocket
+        InputStream inputStream = clientSocket.getInputStream();
+
+        // Create output stream for requests to socket
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        // TODO remove this: BufferedReader inFromServer = new BufferedReader(new InputStreamReader(inputStream));
+        // TODO String sentence = inFromUser.readLine();
+
+        /*
+         * Check for which HTTP command to execute
+         */
+
+        // Incoming HTTP command from request is GET, so calling get method with
+        // correct path and data output stream
+        System.out.println("HttpCommand: "+HTTPCommand);
+        if (HTTPCommand.equals("GET")) {
+            System.out.println("Test");
+            Get(path,outToServer);
+        }
+
+        // Incoming HTTP command from request is HEAD
+        else if (HTTPCommand.equals("HEAD")) {
+            Head(path,port);
+        }
+
+        // Incoming HTTP command from request is PUT
+        else if (HTTPCommand.equals("PUT")) {
+            Put(path,port);
+        }
+
+        // Incoming HTTP command from request is POST
+        else if (HTTPCommand.equals("POST")) {
+            Post(path,port);
+        }
+
+        /*
+         * The input stream reads bytes. The following will code will check the available bytes in the input stream.
+         * If it is larger than the initialized bytes array than we will chop the inputstream data up in smaller parts
+         * of 100 bytes.
+         */
+
+
+
+
+
+        inputStream.read();
+        System.out.println("Available ervoor: "+inputStream.available()   );
+        int size = inputStream.available();
+        bytes = new byte[size];
+        int len = inputStream.read(bytes, 0, size);
+        System.out.println("Available erna: "+inputStream.available()   );
+
+        System.out.println("len: "+len);
+        //
+               /*
+        if (inputStream.available() < 100) {
+            System.out.println("Available: "+inputStream.available());
+            inputStream.read(bytes);
+        }
+
+        else {
+            System.out.println("Not Available "+inputStream.available());
+            int j = 0;
+            String hundredbyte;
+            System.out.println("Modulo: "+(inputStream.available() % 100));
+            for (int i = 0;i < (inputStream.available() % 100) ; i++) {
+                System.out.println("Not Available "+inputStream.available());
+                System.out.println("I: "+i);
+                inputStream.read(bytes, i * 100, 100);
+                hundredbyte = new String(bytes, StandardCharsets.UTF_8);
+                byteString += hundredbyte;
+                j += 1;
+                bytes = new byte[100];
+            }
+            inputStream.read(bytes, (j + 1) * 100, inputStream.available());
+            byteString += new String(bytes, StandardCharsets.UTF_8);
+        }
+        System.out.println("ByteString: "+byteString);  */
+/*
+        //byte[] bufferText = IOUtils.toByteArray(inputStream);
+        //inputStream.read(bufferText);
+        String str = new String(bytes, StandardCharsets.UTF_8);
+        //System.out.println("string: "+str);
+
+        while (!ended) {
             String line = inFromServer.readLine();
-            //System.out.println(line);
+            System.out.println(line);
             if (line.length() > 6) {
                 substr = line.substring(line.length() - 7, line.length());
             }
@@ -45,7 +142,7 @@ class HTTPClient {
                 ended = true;
             }
             htmlInput += line;
-        }*/
+        }
         //System.out.println(htmlInput);
 
         int titlestart=0, titleend=0, bodystart=0, bodyend=0;
@@ -53,10 +150,8 @@ class HTTPClient {
             Character c = (htmlInput.charAt(i));
             int imageEnd = 0;
             if (c.equals('<')) {
-                //System.out.println("< found"+" "+htmlInput.substring(i+1,i+7));
                 if (htmlInput.substring(i+1,i+7).equals("TITLE>") || htmlInput.substring(i+1,i+7).equals("title>")) {
                     titlestart = i+7;
-                    //System.out.println(htmlInput.charAt(titlestart));
                 }
             }
             if (c.equals('<')) {
@@ -89,39 +184,18 @@ class HTTPClient {
         System.out.println("Image: "+images+" length: "+images.size());
 
         if (images.size() != 0) {
-            System.out.println("********test***********");
             outToServer.writeBytes("GET /faq.png HTTP/1.1\n");
             outToServer.writeBytes("Host: tinyos.net \n");
             outToServer.writeBytes("\n");
             for (int i=0;i <images.size();i++) {
                 byte[] buffer = new byte[4096];
-                String img = images.get(i);
                 inputStream.read(buffer);
-                // for each byte in the buffer
-                System.out.println("******************");
-                System.out.println(buffer.length);
-                /*for(byte b:buffer) {
-
-                    // convert byte to character
-                    char c = (char)b;
-
-                    // prints character
-                    System.out.print(c);
-                }
-                System.out.println("\n");*/
-                //BufferedImage imageBuf = ImageIO.read(new ByteArrayInputStream(buffer));
-                //System.out.println("ImageBuff "+ imageBuf);
                 ByteArrayInputStream bis = new ByteArrayInputStream(buffer);
-                System.out.println("bis: "+bis);
                 BufferedImage bImage2 = ImageIO.read(bis);
-                System.out.println("bImage2: "+bImage2);
                 ImageIO.write(bImage2, "jpg", new File("output.jpg") );
-                System.out.println("image created");
-                //System.out.println("Inputstream reading: "+ inputStream.read());
             }
         }
 
-        File htmlTemplateFile = new File("src/template.html");
         String htmlString = FileUtils.readFileToString(htmlTemplateFile);
         String title = htmlInput.substring(titlestart, titleend);
         String body = htmlInput.substring(bodystart, bodyend);
@@ -132,7 +206,25 @@ class HTTPClient {
         File newHtmlFile = new File("src/new.html");
         FileUtils.writeStringToFile(newHtmlFile, htmlString);
 
-
+*/
         clientSocket.close();
+    }
+
+    public static void Get(String path,DataOutputStream outToServer) throws IOException {
+        outToServer.writeBytes("GET /index.html HTTP/1.1\n");
+        outToServer.writeBytes("Host: "+path+" \n");
+        outToServer.writeBytes("\n");
+    }
+
+    public static void Head(String path, int port) {
+
+    }
+
+    public static void Put(String path, int port) {
+
+    }
+
+    public static void Post(String path, int port) {
+
     }
 }
