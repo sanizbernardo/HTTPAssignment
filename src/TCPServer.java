32a98f9b4dfa60d391ea0540c7b6780ee7f18b26
Bernardo Saniz <sanizbernardo@gmail.com>
2018-03-18 13:03:09 +0100

    Just need to fix up error messages, and put and post

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -13,6 +13,8 @@ class TCPServer
     {
         ServerSocket welcomeSocket = new ServerSocket(80);
         int i = 0;
+
+        // always true -> server remains active
         while(true)
         {
             Socket connectionSocket = welcomeSocket.accept();
@@ -31,6 +33,7 @@ class TCPServer
         Socket socket;
         private Handler(Socket socket)
         { this.socket = socket; }
+
         @Override
         public void run()
         {
@@ -38,8 +41,10 @@ class TCPServer
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
 
+                //Confirm thread has been startedd
                 outToClient.writeBytes("Connected\n");
 
+                //
                 boolean requestcomplete = false;
                 List<String> input = new ArrayList<String>();
 
@@ -54,54 +59,65 @@ class TCPServer
                 String[] lines = new String[input.size()];
                 input.toArray(lines);
 
+                //Split request line into method, target file and http version
                 String[] requestline = lines[0].split("\\s+");
 
+                //Check if request is valid, give error message otherwise
+                if (!isValidRequest(requestline, outToClient)) {
+                    outToClient.writeBytes("400 Bad Request");
+                    //throw error
+                }
+
+                //get current date
                 DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
                 LocalDateTime now = LocalDateTime.now();
                 String date = format.format(now);
 
-                File f = new File("src" + requestline[1]);
-                if(!f.exists() || f.isDirectory()) {
-                    outToClient.writeBytes("404 Not found");
-                    //Throw error
-                }
-
+                //Choose correct method
                 switch (requestline[0]) {
                     case "HEAD":
                         confirm(outToClient, requestline[2]);
-                        head(outToClient, lines, date);
+                        head(outToClient, requestline, date);
                         break;
                     case "GET":
                         confirm(outToClient, requestline[2]);
-                        get(outToClient, lines, date);
+                        get(outToClient, requestline, date);
                         break;
                     case "PUT":
                         confirm(outToClient, requestline[2]);
-                        put(outToClient, lines, date);
+                        put(outToClient, requestline, date);
                         break;
                     case "POST":
                         confirm(outToClient, requestline[2]);
-                        post(outToClient, lines, date);
+                        post(outToClient, requestline, date);
                         break;
                     case "DELETE":
                         confirm(outToClient, requestline[2]);
-                        delete(outToClient, lines, date);
+                        delete(outToClient, requestline, date);
                         break;
                     default:
                         outToClient.writeBytes("400 Bad Request");
+                        //throw error
                         break;
                 }
             } catch(IOException e) {
 
             }
-
         }
 
-        private boolean isValidRequest(String[] request) {
+        private boolean isValidRequest(String[] request, DataOutputStream out) throws  IOException{
             boolean b = true;
-            if (request.length != 3 || !request[2].equals("HTTP/1.1")) {
+
+            //Request always contains method and file. Http version is optional, default to HTTP 1.0. Never more than 3
+            //arguments in request line
+            if (request.length < 2 || request.length > 3) {
+                b = false;
+            } else if (request.length ==  2) {
+                request[2] = "HTTP/1.0";
+            } else if (!request[2].equals("HTTP/1.1")) {
                 b = false;
             }
+
             return  b;
         };
 
@@ -111,9 +127,17 @@ class TCPServer
 
         private void head(DataOutputStream out, String[] request, String date) throws  IOException {
             System.out.println("Head detected");
-            File htmlFile = new File("src/index.html");
+
+            //Check if file already exists
+            File htmlFile = new File("src" + request[1]);
+            if(!htmlFile.exists() || htmlFile.isDirectory()) {
+                out.writeBytes("404 Not found");
+                //Throw error
+            }
+
             String htmlString = FileUtils.readFileToString(htmlFile);
 
+            //Output headers
             out.writeBytes("Content-Type: " + "\n");
             out.writeBytes("Content-Length: " + htmlString.length() + "\n");
             out.writeBytes("Date: " + date + "\n");
@@ -122,36 +146,59 @@ class TCPServer
 
         private void get(DataOutputStream out, String[] request, String date) throws  IOException {
             System.out.println("Get detected");
-            File htmlFile = new File("src/index.html");
+
+            //Check if file already exists
+            File htmlFile = new File("src" + request[1]);
+            if(!htmlFile.exists() || htmlFile.isDirectory()) {
+                out.writeBytes("404 Not found");
+                //Throw error
+            }
+
             String htmlString = FileUtils.readFileToString(htmlFile);
 
+            //output headers
             out.writeBytes("Content-Type: " + "\n");
             out.writeBytes("Content-Length: " + htmlString.length() + "\n");
             out.writeBytes("Date: " + date + "\n");
             out.writeBytes("\n");
+
+            //output requested file
             out.writeBytes(htmlString);
         };
 
         private void put(DataOutputStream out, String[] request, String date) throws  IOException {
             System.out.println("Put detected");
+
             out.writeBytes("Date: " + date + "\n");
             out.writeBytes("\n");
         };
 
         private void post(DataOutputStream out, String[] request, String date) throws  IOException {
             System.out.println("Post detected");
+
             out.writeBytes("Date: " + date + "\n");
             out.writeBytes("\n");
 
         };
 
         private  void delete(DataOutputStream out, String[] request, String date) throws  IOException {
-            File htmlFile = new File("src/deletemessage.html");
-            String htmlString = FileUtils.readFileToString(htmlFile);
-
             System.out.println("Delete detected");
+
+            //Check if file already exists
+            File htmlFile = new File("src" + request[1]);
+            if(!htmlFile.exists() || htmlFile.isDirectory()) {
+                out.writeBytes("404 Not found");
+                //Throw error
+            }
+
+            //Fetch delete message
+            File confirmdelete = new File("src/deletemessage.html");
+            String htmlString = FileUtils.readFileToString(confirmdelete);
+
             out.writeBytes("Date: " + date + "\n");
             out.writeBytes("\n");
+
+            //output delete message
             out.writeBytes(htmlString);
         };
 
